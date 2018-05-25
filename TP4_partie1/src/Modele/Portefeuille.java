cbf45544253447d91ce5e7a56fe5b9aa17bdbae3
AntoineB95 <antoinebayemcy@gmail.com>
2018-05-25 17:16:26 +0200

    1.5+1.6

--- TP4_partie1/src/Modele/Portefeuille.java
+++ TP4_partie1/src/Modele/Portefeuille.java
@@ -10,9 +10,8 @@ import java.util.*;
  *
  * @author Feray Beaumont
  */
-public class Portefeuille 
-{
-    
+public class Portefeuille extends Exception
+{   
     public HashMap<String, Fonds> hFonds;
     
     public HashMap<String, Instrument> hInstrument;
@@ -33,5 +32,60 @@ public class Portefeuille
     {
         hInstrument=new HashMap<>();
         hInstrument.put(x,i);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public void ajouterFond(String x, double a)
+    {
+        try
+        {
+            if(hFonds.get(x)!=null)
+            {
+                throw new Exception("FondsExistant");
+            }
+        }
+        catch(Exception e)
+        {
+            Fonds f = new Fonds(a);
+            hFonds.put(x,f);
+        }
+        
+    }
+    
+    public void ajouterInstrument(String x, Fonds f)
+    {
+        Instrument i = hInstrument.get(x);
+        i.ajouter(f);
     }
 }
